6518cfe89937eda48edbbd37582b413378601699
jeong <ibory1220@naver.com>
2023-04-22 17:50:37 +0100

    Init

---
+++ src/SPAModule/App.js
@@ -0,0 +1,8 @@
+export class App {
+  constructor() {}
+
+  static render(target, element) {
+    target.appendChild(element);
+  }
+  static createElement() {}
+}
---
+++ src/main.js
@@ -0,0 +1,3 @@
+import { App } from './SPAModule/App';
+
+const rootElement = document.querySelector('#root');
